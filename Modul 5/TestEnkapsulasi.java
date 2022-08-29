a4e1f6893ad591f224431b5660a8eb4da9406a1c
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-08-29 08:01:07 +0700

    Delete TestEnkapsulasi.java

--- Modul 5/TestEnkapsulasi.java
+++
@@ -1,9 +0,0 @@
-public class TestEnkapsulasi {
-	public static void main(String[] args) {
-		Pegawai dataPeg=new Pegawai();
-		dataPeg.setNama("Budi");
-		dataPeg.setJabatan("Supervisor");
-		dataPeg.setGaji(4000000);
-		dataPeg.cetakPegawai();
-	}
-}
