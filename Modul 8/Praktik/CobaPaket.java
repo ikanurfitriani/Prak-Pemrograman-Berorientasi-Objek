6dc458dd754d352253cf25541ff8b3809937fed6
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-08-29 08:28:29 +0700

    Delete Modul 8/Praktik directory

--- Modul 8/Praktik/CobaPaket.java
+++
@@ -1,9 +0,0 @@
-import pegawai.*;
-public class CobaPaket {
-	public static void main(String args[]) {
-		Paket coba=new Paket();
-		coba.isiPaket();
-		coba.setData(20, "Ika Nurfitriani");
-		coba.info();
-	}
-}
