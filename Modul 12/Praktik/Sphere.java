5e7edaec33019fed103c055ff868b551010a554e
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-08-29 08:34:36 +0700

    Delete Modul 12/Praktik directory

--- Modul 12/Praktik/Sphere.java
+++
@@ -1,9 +0,0 @@
-public class Sphere extends RoundShape {
-    public Sphere(int xCenter, int yCenter, float radius) {
-        super(xCenter, yCenter, radius);
-    }
-    public float area() {
-        float surfaceArea = (float) (4.0*Math.PI*Math.pow(radiusOfCircle, 2.0));
-        return surfaceArea;
-    }
-}
