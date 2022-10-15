2b8d3c80070a775772fd769116b65a25c96410d9
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 16:13:57 +0700

    Delete Modul 11 directory

--- Modul 11/Tugas/BilanganGanjil.java
+++
@@ -1,7 +0,0 @@
-public class BilanganGanjil {
-	public static void main(String[] args) throws Exception {
-		int[] b = {1, 3, 5, 7, 9};
-		System.out.println("=== BILANGAN GANJIL ===");
-		System.out.println("Data kedua: " + b[1]);
-	}
-}
--- Modul 11/Tugas/BilanganGenap.java
+++
@@ -1,7 +0,0 @@
-public class BilanganGenap {
-	public static void main(String[] args) throws Exception {
-		int[] a = {2, 4, 6, 8, 10};
-		System.out.println("=== BILANGAN GENAP ===");
-		System.out.println("Data keempat: " + a[3]);
-	}
-}
